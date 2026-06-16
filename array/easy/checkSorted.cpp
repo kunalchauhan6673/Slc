960e8d828971e0df82b9794388d82c92f5fb1857
Kunal Chauhan <114133835+kunalchauhan6673@users.noreply.github.com>
2026-06-16 22:20:16 +0530

    checkSorted

---
+++ array/easy/checkSorted.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkSorted(int a[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        if (a[i] > a[i+1]) {
+                return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int a[] = {10, 100, 2, 34, 32, 1000};
+
+    int n = sizeof(a) / sizeof(a[0]);
+
+    cout << checkSorted(a, n);
+
+    return 0;
+}
+
